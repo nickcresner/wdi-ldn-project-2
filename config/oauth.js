79fa25983553a19551c90cc3c3164cd75be9666d
Nick Cresner <nickcresner@gmail.com>
2017-06-15 14:41:30 +0100

    redeploying

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ module.exports = {
   facebook: {
     loginUrl: 'https://www.facebook.com/v2.9/dialog/oauth',
     clientId: process.env.FACEBOOK_APP_ID,
-    redirectUri: 'http://localhost:8000/oauth/facebook',
+    redirectUri: 'https://shielded-earth-17510.herokuapp.com/oauth/facebook',
     clientSecret: process.env.FACEBOOK_APP_SECRET,
     scope: 'email, user_hometown',
     accessTokenUrl: 'https://graph.facebook.com/v2.9/oauth/access_token',
